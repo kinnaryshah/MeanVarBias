6309ab0ae1c873bb99489d51f280c860db2e3599
kinnaryshah <kinnaryshahh@gmail.com>
2024-01-29 15:03:14 -0500

    SPARKX code for 2000 genes simulation

---
+++ R/simulations/all_SVGs_2000/sparkx.R
@@ -0,0 +1,13 @@
+library(SPARK)
+library(SpatialExperiment)
+spe <- readRDS(file = "spe_simulation.rds")
+
+sp_count <- counts(spe)
+location <- spatialCoords(spe)
+sparkX_unweighted <- sparkx(sp_count,location,numCores=1,option="mixture")
+
+load(file = "weights.Rdata")
+weighted_sp_count <- t(weights)*sp_count
+sparkX_weighted <- sparkx(weighted_sp_count,location,X_in=weights,numCores=2,option="mixture")
+
+save(sparkX_unweighted, sparkX_weighted, file="simulation_weighted_SPARKX.Rdata")
