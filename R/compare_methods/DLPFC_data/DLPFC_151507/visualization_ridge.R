6a7f08bbe07cd97cc4540b37217eb626a85ed96f
Kinnary Shah <kinnaryshahh@gmail.com>
2024-02-20 09:32:13 -0500

    density plots

--- R/compare_methods/DLPFC_data/DLPFC_151507/visualization_ridge.R
+++ R/compare_methods/DLPFC_data/DLPFC_151507/visualization_ridge.R
@@ -26,19 +26,6 @@ df <- data.frame(
                                      quantile(mean, probs=0:9/10))) %>%
   tibble::rownames_to_column() 
 
-HVG_ridge_combined <- df %>% 
-  mutate(quantile = as.factor(quantile)) |> 
-  ggplot() +
-  geom_density_ridges2(aes(x = rank, y = quantile), rel_min_height = 0.02) +
-  theme_ridges(grid = TRUE) +
-  labs(
-    y = "quantile - mean of logcounts",
-    x = "rank",
-    title = "HVG combined"
-  ) +
-  coord_cartesian(xlim = c(1, n_genes)) +
-  theme_bw()
-
 df_signal <- df %>%
   mutate(quantile = as.factor(quantile)) %>%
   group_by(quantile) %>%
@@ -53,9 +40,8 @@ df_background <- df[-indices,] %>%
 
 df <- rbind(df_signal, df_background)
 
-HVG_ridge_signal <- ggplot(df, aes(x = rank, y = quantile)) +
-  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3,
-                       stat = "binline", bins = 75, draw_baseline = FALSE) +
+HVG_ridge <- ggplot(df, aes(x = rank, y = quantile)) +
+  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3) +
   theme_ridges(grid = TRUE) +
   labs(
     y = "decile - mean of logcounts",
@@ -65,8 +51,6 @@ HVG_ridge_signal <- ggplot(df, aes(x = rank, y = quantile)) +
   guides(fill=guide_legend(title="group")) +
   coord_cartesian(xlim = c(1, n_genes)) +
   theme_bw() 
-
-HVG_ridge <- wrap_plots(HVG_ridge_combined, HVG_ridge_signal)
 
 #ridge plots separated by noise and signal for MoransI
 frac <- round(dim(spe_MoransI)[1]*0.1*0.1)
@@ -79,19 +63,6 @@ df <- data.frame(
                                      quantile(mean, probs=0:9/10))) %>%
   tibble::rownames_to_column()
 
-MoransI_ridge_combined <- df %>% 
-  mutate(quantile = as.factor(quantile)) |> 
-  ggplot() +
-  geom_density_ridges2(aes(x = rank, y = quantile), rel_min_height = 0.02) +
-  theme_ridges(grid = TRUE) +
-  labs(
-    y = "quantile - mean of logcounts",
-    x = "rank",
-    title = "MoransI combined"
-  ) +
-  coord_cartesian(xlim = c(1, n_genes)) +
-  theme_bw()
-
 df_signal <- df %>%
   mutate(quantile = as.factor(quantile)) %>%
   group_by(quantile) %>%
@@ -106,9 +77,8 @@ df_background <- df[-indices,] %>%
 
 df <- rbind(df_signal, df_background)
 
-MoransI_ridge_signal <- ggplot(df, aes(x = rank, y = quantile)) +
-  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3,
-                       stat = "binline", bins = 75, draw_baseline = FALSE) +
+MoransI_ridge <- ggplot(df, aes(x = rank, y = quantile)) +
+  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3) +
   theme_ridges(grid = TRUE) +
   labs(
     y = "decile - mean of logcounts",
@@ -118,8 +88,6 @@ MoransI_ridge_signal <- ggplot(df, aes(x = rank, y = quantile)) +
   guides(fill=guide_legend(title="group")) +
   coord_cartesian(xlim = c(1, n_genes)) +
   theme_bw() 
-
-MoransI_ridge <- wrap_plots(MoransI_ridge_combined, MoransI_ridge_signal)
 
 #ridge plots separated by noise and signal for nnSVG
 frac <- round(dim(spe_nnSVG)[1]*0.1*0.1)
@@ -131,20 +99,6 @@ df <- data.frame(
                                      quantile(mean, probs=0:9/10))) %>%
   tibble::rownames_to_column()
 
-
-nnSVG_ridge_combined <- df %>% 
-  mutate(quantile = as.factor(quantile)) |> 
-  ggplot() +
-  geom_density_ridges2(aes(x = rank, y = quantile), rel_min_height = 0.02) +
-  theme_ridges(grid = TRUE) +
-  labs(
-    y = "quantile - mean of logcounts",
-    x = "rank",
-    title = "nnSVG combined"
-  ) +
-  coord_cartesian(xlim = c(1, n_genes)) +
-  theme_bw()
-
 df_signal <- df %>%
   mutate(quantile = as.factor(quantile)) %>%
   group_by(quantile) %>%
@@ -159,9 +113,8 @@ df_background <- df[-indices,] %>%
 
 df <- rbind(df_signal, df_background)
 
-nnSVG_ridge_signal <- ggplot(df, aes(x = rank, y = quantile)) +
-  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3,
-                       stat = "binline", bins = 75, draw_baseline = FALSE) +
+nnSVG_ridge <- ggplot(df, aes(x = rank, y = quantile)) +
+  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3) +
   theme_ridges(grid = TRUE) +
   labs(
     y = "decile - mean of logcounts",
@@ -171,8 +124,6 @@ nnSVG_ridge_signal <- ggplot(df, aes(x = rank, y = quantile)) +
   guides(fill=guide_legend(title="group")) +
   coord_cartesian(xlim = c(1, n_genes)) +
   theme_bw() 
-
-nnSVG_ridge <- wrap_plots(nnSVG_ridge_combined, nnSVG_ridge_signal)
 
 #ridge plots separated by noise and signal for SPARKX
 frac <- round(dim(spe_SPARKX)[1]*0.1*0.1)
@@ -185,20 +136,6 @@ df <- data.frame(
                                      quantile(mean, probs=0:9/10))) %>%
   tibble::rownames_to_column()
 
-
-SPARKX_ridge_combined <- df %>% 
-  mutate(quantile = as.factor(quantile)) |> 
-  ggplot() +
-  geom_density_ridges2(aes(x = rank, y = quantile), rel_min_height = 0.02) +
-  theme_ridges(grid = TRUE) +
-  labs(
-    y = "quantile - mean of logcounts",
-    x = "rank",
-    title = "SPARKX combined"
-  ) +
-  coord_cartesian(xlim = c(1, n_genes)) +
-  theme_bw()
-
 df_signal <- df %>%
   mutate(quantile = as.factor(quantile)) %>%
   group_by(quantile) %>%
@@ -213,9 +150,8 @@ df_background <- df[-indices,] %>%
 
 df <- rbind(df_signal, df_background)
 
-SPARKX_ridge_signal <- ggplot(df, aes(x = rank, y = quantile)) +
-  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3,
-                       stat = "binline", bins = 75, draw_baseline = FALSE) +
+SPARKX_ridge <- ggplot(df, aes(x = rank, y = quantile)) +
+  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3) +
   theme_ridges(grid = TRUE) +
   labs(
     y = "decile - mean of logcounts",
@@ -225,8 +161,6 @@ SPARKX_ridge_signal <- ggplot(df, aes(x = rank, y = quantile)) +
   guides(fill=guide_legend(title="group")) +
   coord_cartesian(xlim = c(1, n_genes)) +
   theme_bw() 
-
-SPARKX_ridge <- wrap_plots(SPARKX_ridge_combined, SPARKX_ridge_signal)
 
 #ridge plots separated by noise and signal for SpaGFT
 frac <- round(dim(spe_HVG)[1]*0.1*0.1)
@@ -242,19 +176,6 @@ df <- data.frame(
                                      quantile(mean, probs=0:9/10))) %>%
   tibble::rownames_to_column()
 
-SpaGFT_ridge_combined <- df %>% 
-  mutate(quantile = as.factor(quantile)) |> 
-  ggplot() +
-  geom_density_ridges2(aes(x = rank, y = quantile), rel_min_height = 0.02) +
-  theme_ridges(grid = TRUE) +
-  labs(
-    y = "quantile - mean of logcounts",
-    x = "rank",
-    title = "SpaGFT combined"
-  ) +
-  coord_cartesian(xlim = c(1, n_genes)) +
-  theme_bw()
-
 df_signal <- df %>%
   mutate(quantile = as.factor(quantile)) %>%
   group_by(quantile) %>%
@@ -269,9 +190,8 @@ df_background <- df[-indices,] %>%
 
 df <- rbind(df_signal, df_background)
 
-SpaGFT_ridge_signal <- ggplot(df, aes(x = rank, y = quantile)) +
-  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3,
-                       stat = "binline", bins = 75, draw_baseline = FALSE) +
+SpaGFT_ridge <- ggplot(df, aes(x = rank, y = quantile)) +
+  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3) +
   theme_ridges(grid = TRUE) +
   labs(
     y = "decile - mean of logcounts",
@@ -281,8 +201,6 @@ SpaGFT_ridge_signal <- ggplot(df, aes(x = rank, y = quantile)) +
   guides(fill=guide_legend(title="group")) +
   coord_cartesian(xlim = c(1, n_genes)) +
   theme_bw() 
-
-SpaGFT_ridge <- wrap_plots(SpaGFT_ridge_combined, SpaGFT_ridge_signal)
 
 #ridge plots separated by noise and signal for SpatialDE2
 frac <- round(dim(spe_HVG)[1]*0.1*0.1)
@@ -300,19 +218,6 @@ df <- data.frame(
                                      quantile(mean, probs=0:9/10))) %>%
   tibble::rownames_to_column()
 
-SpatialDE2_ridge_combined <- df %>% 
-  mutate(quantile = as.factor(quantile)) |> 
-  ggplot() +
-  geom_density_ridges2(aes(x = rank, y = quantile), rel_min_height = 0.02) +
-  theme_ridges(grid = TRUE) +
-  labs(
-    y = "quantile - mean of logcounts",
-    x = "rank",
-    title = "SpatialDE2 combined"
-  ) +
-  coord_cartesian(xlim = c(1, n_genes)) +
-  theme_bw()
-
 df_signal <- df %>%
   mutate(quantile = as.factor(quantile)) %>%
   group_by(quantile) %>%
@@ -327,9 +232,8 @@ df_background <- df[-indices,] %>%
 
 df <- rbind(df_signal, df_background)
 
-SpatialDE2_ridge_signal <- ggplot(df, aes(x = rank, y = quantile)) +
-  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3,
-                       stat = "binline", bins = 75, draw_baseline = FALSE) +
+SpatialDE2_ridge <- ggplot(df, aes(x = rank, y = quantile)) +
+  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3) +
   theme_ridges(grid = TRUE) +
   labs(
     y = "decile - mean of logcounts",
@@ -340,12 +244,11 @@ SpatialDE2_ridge_signal <- ggplot(df, aes(x = rank, y = quantile)) +
   coord_cartesian(xlim = c(1, n_genes)) +
   theme_bw() 
 
-SpatialDE2_ridge <- wrap_plots(SpatialDE2_ridge_combined, SpatialDE2_ridge_signal)
-
-
-pdf("visualization_ridge_2.pdf", width = 21, height = 20)
+pdf("visualization_ridge.pdf", width = 21, height = 20)
 HVG_ridge
 MoransI_ridge
 nnSVG_ridge
 SPARKX_ridge
+SpaGFT_ridge
+SpatialDE2_ridge
 dev.off()
