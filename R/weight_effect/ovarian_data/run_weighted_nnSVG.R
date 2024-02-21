467c28baefdf694b8d54edbf25601e918fdf17ad
Kinnary Shah <kinnaryshahh@gmail.com>
2024-02-21 12:41:20 -0500

    run weighted nnSVG on ovarian data

---
+++ R/weight_effect/ovarian_data/run_weighted_nnSVG.R
@@ -0,0 +1,30 @@
+library(nnSVG)
+library(SpatialExperiment)
+library(BRISC)
+library(BiocParallel)
+library(scuttle)
+library(Matrix)
+library(spoon)
+
+spe <- readVisium("~/ovarian_cancer_forrest/outs")
+
+dim(spe)
+
+## Remove genes without enough data
+spe <- filter_genes(spe, 2, 0.2)
+
+dim(spe)
+
+spe <- logNormCounts(spe)
+
+weights <- generate_weights(input = spe,
+                            stabilize = TRUE,
+                            BPPARAM = MulticoreParam(workers = 10,
+                                                     RNGseed = 4))
+
+spe <- weighted_nnSVG(input = spe,
+                      w = weights,
+                      BPPARAM = MulticoreParam(workers = 10, RNGseed = 5))
+
+file = "spe_weighted_nnSVG.rds"
+saveRDS(spe, file = file)
