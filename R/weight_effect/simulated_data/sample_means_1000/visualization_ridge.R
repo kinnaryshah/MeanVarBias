eb5ab653148743c7bb631bfb6987fb658e2f59d0
Kinnary Shah <kinnaryshahh@gmail.com>
2024-02-26 11:05:35 -0500

    vis weighted effect on sim

---
+++ R/weight_effect/simulated_data/sample_means_1000/visualization_ridge.R
@@ -0,0 +1,127 @@
+library(ggplot2)
+library(SpatialExperiment)
+library(patchwork)
+library(GGally)
+library(dplyr)
+library(ggridges)
+
+spe_unweighted <- readRDS("../../../compare_methods/simulated_data/sample_means_1000/spe_nnSVG.rds")
+spe_weighted <- readRDS("spe_weighted_nnSVG.rds")
+
+n_genes <- dim(spe_weighted)[1]
+
+#ridge plots separated by noise and signal for weighted
+frac <- round(dim(spe_weighted)[1]*0.1*0.1)
+
+df <- data.frame(
+  rank = rowData(spe_weighted)$weighted_rank,
+  mean = rowData(spe_weighted)$weighted_mean
+) %>% mutate(quantile = findInterval(mean, 
+                                     quantile(mean, probs=0:9/10))) %>%
+  tibble::rownames_to_column()
+
+df_signal <- df %>%
+  mutate(quantile = as.factor(quantile)) %>%
+  group_by(quantile) %>%
+  slice_min(order_by = rank, n = frac) %>%
+  mutate(grp = "signal")
+
+indices <- as.integer(df_signal$rowname)
+
+df_background <- df[-indices,] %>%
+  mutate(quantile = as.factor(quantile)) %>%
+  mutate(grp = "background")
+
+df <- rbind(df_signal, df_background)
+
+weighted_ridge <- ggplot(df, aes(x = rank, y = quantile)) +
+  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3) +
+  theme_ridges(grid = TRUE) +
+  labs(
+    y = "decile - w mean of logcounts",
+    x = "rank",
+    title = "weighted nnSVG signal"
+  ) +
+  guides(fill=guide_legend(title="group")) +
+  coord_cartesian(xlim = c(1, n_genes)) +
+  theme_bw() 
+
+#ridge plots separated by noise and signal for unweighted
+
+df <- data.frame(
+  rank = rowData(spe_unweighted)$rank,
+  mean = rowData(spe_unweighted)$mean
+) %>% mutate(quantile = findInterval(mean, 
+                                     quantile(mean, probs=0:9/10))) %>%
+  tibble::rownames_to_column()
+
+df_signal <- df %>%
+  mutate(quantile = as.factor(quantile)) %>%
+  group_by(quantile) %>%
+  slice_min(order_by = rank, n = frac) %>%
+  mutate(grp = "signal")
+
+indices <- as.integer(df_signal$rowname)
+
+df_background <- df[-indices,] %>%
+  mutate(quantile = as.factor(quantile)) %>%
+  mutate(grp = "background")
+
+df <- rbind(df_signal, df_background)
+
+unweighted_ridge <- ggplot(df, aes(x = rank, y = quantile)) +
+  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3) +
+  theme_ridges(grid = TRUE) +
+  labs(
+    y = "decile - unw mean of logcounts",
+    x = "rank",
+    title = "unweighted nnSVG signal"
+  ) +
+  guides(fill=guide_legend(title="group")) +
+  coord_cartesian(xlim = c(1, n_genes)) +
+  theme_bw()
+
+vis_1 <- wrap_plots(weighted_ridge, unweighted_ridge, ncol = 2, guides = 'collect')
+
+#ridge plots separated by noise and signal for weighted
+frac <- round(dim(spe_weighted)[1]*0.1*0.1)
+
+df <- data.frame(
+  rank = rowData(spe_weighted)$weighted_rank,
+  mean = rowData(spe_unweighted)$mean
+) %>% mutate(quantile = findInterval(mean, 
+                                     quantile(mean, probs=0:9/10))) %>%
+  tibble::rownames_to_column()
+
+df_signal <- df %>%
+  mutate(quantile = as.factor(quantile)) %>%
+  group_by(quantile) %>%
+  slice_min(order_by = rank, n = frac) %>%
+  mutate(grp = "signal")
+
+indices <- as.integer(df_signal$rowname)
+
+df_background <- df[-indices,] %>%
+  mutate(quantile = as.factor(quantile)) %>%
+  mutate(grp = "background")
+
+df <- rbind(df_signal, df_background)
+
+weighted_ridge <- ggplot(df, aes(x = rank, y = quantile)) +
+  geom_density_ridges2(aes(fill = grp), rel_min_height = 0.02, alpha = 0.3) +
+  theme_ridges(grid = TRUE) +
+  labs(
+    y = "decile - unw mean of logcounts",
+    x = "rank",
+    title = "weighted nnSVG signal"
+  ) +
+  guides(fill=guide_legend(title="group")) +
+  coord_cartesian(xlim = c(1, n_genes)) +
+  theme_bw() 
+
+vis_2 <- wrap_plots(weighted_ridge, unweighted_ridge, ncol = 2, guides = 'collect')
+
+pdf("visualization_ridge.pdf", width = 21, height = 20)
+vis_1
+vis_2
+dev.off()
