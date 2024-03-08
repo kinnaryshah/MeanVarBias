f9ac7def315625358962d097dd4f1f2bd4bad8a8
Kinnary Shah <kinnaryshahh@gmail.com>
2024-03-08 11:54:26 -0500

    weighted and unweighted nnSVG for breast cancer data

---
+++ R/weight_effect/breast_data/run_weighted_nnSVG.R
@@ -0,0 +1,46 @@
+library(nnSVG)
+library(SpatialExperiment)
+library(BRISC)
+library(BiocParallel)
+library(scuttle)
+library(Matrix)
+library(spoon)
+
+
+(spe <- read10xVisium("/users/kshah/mean_var_project/breast_cancer_data/outs",
+                      type = "sparse",   # use sparse (not HDF5) format
+                      data = "raw",     
+                      images = "lowres", # specify which image(s) to include
+                      load = TRUE))      # specify whether or not to load image(s)
+
+#row data did not load in the proper format
+rowData(spe)$gene_id <- rownames(spe)
+rowData(spe)$gene_name <- rowData(spe)$symbol
+rowData(spe)$symbol <- NULL
+
+dim(spe)
+
+#convert counts matrix from sparse matrix to matrix
+#weighted_nnSVG requires a matrix for calculating weighted_logcounts matrix
+counts(spe) <- as.matrix(counts(spe))
+
+## Remove genes without enough data
+spe <- filter_genes(spe, 2, 0.2)
+
+dim(spe)
+
+spe <- logNormCounts(spe)
+
+weights <- generate_weights(input = spe,
+                            stabilize = TRUE,
+                            BPPARAM = MulticoreParam(workers = 10,
+                                                     RNGseed = 4))
+print(dim(weights))
+saveRDS(weights, file = "weights.rds")
+
+spe <- weighted_nnSVG(input = spe,
+                      w = weights,
+                      BPPARAM = MulticoreParam(workers = 10, RNGseed = 5))
+
+file = "spe_weighted_nnSVG.rds"
+saveRDS(spe, file = file)
