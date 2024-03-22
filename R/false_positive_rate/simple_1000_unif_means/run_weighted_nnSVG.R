d298b18263d8f6bde7d53fb22d5708bc3b8081e9
Kinnary Shah <kinnaryshahh@gmail.com>
2024-03-22 11:35:25 -0400

    run weighted and unweighted nnSVG for false pos calc

---
+++ R/false_positive_rate/simple_1000_unif_means/run_weighted_nnSVG.R
@@ -0,0 +1,27 @@
+library(nnSVG)
+library(SpatialExperiment)
+library(BRISC)
+library(BiocParallel)
+library(scuttle)
+library(Matrix)
+library(spoon)
+
+fn <- ("../../../../simulations/nonSVGs/simple_1000_unif_means/spe_simulation.rds") 
+
+spe <- readRDS(fn)
+
+dim(spe)
+
+spe <- logNormCounts(spe)
+
+weights <- generate_weights(input = spe,
+                            stabilize = TRUE,
+                            BPPARAM = MulticoreParam(workers = 10,
+                                                     RNGseed = 4))
+
+spe <- weighted_nnSVG(input = spe,
+                      w = weights,
+                      BPPARAM = MulticoreParam(workers = 10, RNGseed = 5))
+
+file = "spe_weighted_nnSVG.rds"
+saveRDS(spe, file = file)
