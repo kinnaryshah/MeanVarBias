398261890ff51ede9de099a0b3daf09c549bdb7f
Kinnary Shah <kinnaryshahh@gmail.com>
2024-04-02 13:24:21 -0400

    use padj for FPR calc

--- R/false_positive_rate/simple_1000/false_positive_rate_calc.R
+++ R/false_positive_rate/simple_1000/false_positive_rate_calc.R
@@ -7,8 +7,6 @@ library(SpatialExperiment)
 
 # 2. compare the false positive rate in the weighted and unweighted version
 
-# 3. plot the false positive rate for the weighted and unweighted version
-
 spe_unweighted <- readRDS("spe_nnSVG.rds")
 spe_weighted <- readRDS("spe_weighted_nnSVG.rds")
 
@@ -17,7 +15,7 @@ spe_weighted <- readRDS("spe_weighted_nnSVG.rds")
 
 # unweighted
 
-pvalues_unweighted <- rowData(spe_unweighted)$pval
+pvalues_unweighted <- rowData(spe_unweighted)$padj
 false_positives_unweighted <- sum(pvalues_unweighted < 0.05)
 total_genes_unweighted <- dim(spe_unweighted)[1]
 false_positive_rate_unweighted <- false_positives_unweighted / total_genes_unweighted
@@ -25,12 +23,13 @@ false_positive_rate_unweighted <- false_positives_unweighted / total_genes_unwei
 # weighted
 
 pvalues_weighted <- 1 - pchisq(rowData(spe_weighted)$weighted_LR_stat, df = 2)
+pvalues_weighted <- p.adjust(pvalues_weighted, method = "BH")
 false_positives_weighted <- sum(pvalues_weighted < 0.05)
 total_genes_weighted <- dim(spe_weighted)[1]
 false_positive_rate_weighted <- false_positives_weighted / total_genes_weighted
 
 # 2. compare the false positive rate in the weighted and unweighted version 
 
-false_positive_rate_unweighted # 0.019
-false_positive_rate_weighted # 0.055
+false_positive_rate_unweighted # 0
+false_positive_rate_weighted # 0.049
 
