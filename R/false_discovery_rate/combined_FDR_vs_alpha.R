22ab28597411d1dacdeb1c6d91088cd89016a3bc
Kinnary Shah <kinnaryshahh@gmail.com>
2024-05-22 16:18:44 -0400

    rearrange files

--- R/false_discovery_rate/combined_FDR_vs_alpha.R
+++ R/false_discovery_rate/combined_FDR_vs_alpha.R
@@ -2,6 +2,58 @@ library(SpatialExperiment)
 library(dplyr)
 library(ggplot2)
 
+# 1. visualize p-value distributions for 5 replicates
+# 2. plot average FDR for different alpha values
+# 3. plot average TPR vs alpha
+# 4. plot average TNR vs alpha
+
+# 1. visualize p-value distributions for 5 replicates
+# separate plots 
+
+for (i in 1:5) {
+  print(i)
+  spe_unweighted <- readRDS(paste0("spe_nnSVG_", i, ".rds"))
+  
+  p1 <- data.frame(pval = rowData(spe_unweighted)$pval) %>%
+    ggplot(aes(x = pval)) +
+    geom_histogram(binwidth = 0.01) +
+    ggtitle(paste0("unweighted p-value distribution for ", i, "0% nonSVGs")) +
+    # add limits to y-axis
+    ylim(0, 1000) +
+    theme_minimal()
+  
+  spe_weighted <- readRDS(paste0("spe_weighted_nnSVG_", i, ".rds"))
+  p2 <- data.frame(pval = 1 - pchisq(rowData(spe_weighted)$weighted_LR_stat, df=2)) %>%
+    ggplot(aes(x = pval)) +
+    geom_histogram(binwidth = 0.01) +
+    ggtitle(paste0("weighted p-value distribution for ", i, "0% nonSVGs")) +
+    # add limits to y-axis
+    ylim(0, 1000) +
+    theme_minimal()
+  
+  # save plots
+  pdf(paste0("p_value_distribution_", i, ".pdf"))
+  print(p1)
+  print(p2)
+  dev.off()
+  
+}
+
+# compute Kolmogorov–Smirnov (K-S) distance between the distribution of the computed p-values and the uniform distribution
+# for each replicate
+ks_unweighted <- c()
+ks_weighted <- c()
+
+for (i in 3:7) {
+  spe_unweighted <- readRDS(paste0("spe_nnSVG_", i, ".rds"))
+  ks_unweighted <- c(ks_unweighted, ks.test(rowData(spe_unweighted)$pval, "punif")$statistic)
+  
+  spe_weighted <- readRDS(paste0("spe_weighted_nnSVG_", i, ".rds"))
+  ks_weighted <- c(ks_weighted, ks.test(1 - pchisq(rowData(spe_weighted)$weighted_LR_stat, df=2), "punif")$statistic)
+}
+
+# 2. plot average FDR for different alpha values
+# one plot combining all replicates
 # make a function with input of spe_unweighted and spe_weighted, return a data frame of fdr_unweighted and fdr_weighted 
 create_FDR_df <- function(spe_unweighted, spe_weighted) {
   
@@ -23,7 +75,7 @@ create_FDR_df <- function(spe_unweighted, spe_weighted) {
     }
     return(fdr)
   })
-
+  
   
   # WEIGHTED
   
@@ -54,46 +106,34 @@ create_FDR_df <- function(spe_unweighted, spe_weighted) {
 # create a column for the fdr_1 to fdr_8 
 
 df <- data.frame()
-for (i in 1:8) {
+for (i in 1:5) {
   print(i)
-  spe_unweighted <- readRDS(paste0("fdr_", i, "/spe_nnSVG.rds"))
-  spe_weighted <- readRDS(paste0("fdr_", i, "/spe_weighted_nnSVG.rds"))
+  spe_unweighted <- readRDS(paste0("spe_nnSVG_", i, ".rds"))
+  spe_weighted <- readRDS(paste0("spe_weighted_nnSVG_", i, ".rds"))
   
   df_temp <- create_FDR_df(spe_unweighted, spe_weighted)
-  df_temp$sim <- i+1
+  df_temp$sim <- i
   
   df <- rbind(df, df_temp)
 }
 
-# add another df_temp to df using i=1 from the folder "../weight_effect/simulated_data/nonSVGs/simple_300_unif_means/"
-spe_unweighted <- readRDS("../weight_effect/simulated_data/nonSVGs/simple_300_unif_means/spe_nnSVG.rds")
-spe_weighted <- readRDS("../weight_effect/simulated_data/nonSVGs/simple_300_unif_means/spe_weighted_nnSVG.rds")
+# average FDR for different alpha values
+df_avg <- df %>%
+  group_by(alpha) %>%
+  summarise(fdr_percent_unweighted = mean(fdr_percent_unweighted),
+            fdr_percent_weighted = mean(fdr_percent_weighted))
 
-df_temp <- create_FDR_df(spe_unweighted, spe_weighted)
-df_temp$sim <- 1
-
-df <- rbind(df, df_temp)
-
-# plot the FDRs for unweighted and weighted models
-# separate plots for each fdr 1 to 9
-# use faceting from ggplot2
-# x axis is the alpha level
-# y axis is the FDR percentage
-# color the lines by the model type (unweighted or weighted)
-
-p <- ggplot(df, aes(x = alpha, y = fdr_percent_unweighted, color = "unweighted")) +
-  geom_line() +
+# plot average FDR for different alpha values
+p_FDR <- df_avg %>%
+  ggplot(aes(x = alpha)) +
+  geom_line(aes(y = fdr_percent_unweighted, color = "unweighted")) +
   geom_line(aes(y = fdr_percent_weighted, color = "weighted")) +
-  facet_wrap(~sim) +
-  labs(title = "FDR vs significance level",
-       x = "Significance level",
-       y = "FDR (%)",
-       color = "Model") +
-  theme_bw()
+  ggtitle("Average FDR vs alpha") +
+  theme_bw() +
+  theme(legend.position = "top")
 
-# save the plot
-ggsave("combined_FDR_vs_alpha.png", p, width = 10, height = 10)
-
+# 3. plot average TPR and TPR vs alpha
+# one plot combining all replicates
 create_TNR_TPR_df <- function(spe_unweighted, spe_weighted) {
   
   fdr_levels <- seq(0.001, 0.1, by = 0.001)  
@@ -139,201 +179,45 @@ create_TNR_TPR_df <- function(spe_unweighted, spe_weighted) {
 }
 
 df <- data.frame()
-for (i in 1:8) {
+for (i in 1:5) {
   print(i)
-  spe_unweighted <- readRDS(paste0("fdr_", i, "/spe_nnSVG.rds"))
-  spe_weighted <- readRDS(paste0("fdr_", i, "/spe_weighted_nnSVG.rds"))
+  spe_unweighted <- readRDS(paste0("spe_nnSVG_", i, ".rds"))
+  spe_weighted <- readRDS(paste0("spe_weighted_nnSVG_", i, ".rds"))
   
   df_temp <- create_TNR_TPR_df(spe_unweighted, spe_weighted)
-  df_temp$sim <- i+1
+  df_temp$sim <- i
   
   df <- rbind(df, df_temp)
 }
 
-# add another df_temp to df using i=1 from the folder "../weight_effect/simulated_data/nonSVGs/simple_300_unif_means/"
-spe_unweighted <- readRDS("../weight_effect/simulated_data/nonSVGs/simple_300_unif_means/spe_nnSVG.rds")
-spe_weighted <- readRDS("../weight_effect/simulated_data/nonSVGs/simple_300_unif_means/spe_weighted_nnSVG.rds")
+# average TPR and TNR for different FDR levels
+df_avg <- df %>%
+  group_by(fdr_levels) %>%
+  summarise(tpr_levels_unweighted = mean(tpr_levels_unweighted),
+            tnr_levels_unweighted = mean(tnr_levels_unweighted),
+            tpr_levels_weighted = mean(tpr_levels_weighted),
+            tnr_levels_weighted = mean(tnr_levels_weighted))
 
-df_temp <- create_TNR_TPR_df(spe_unweighted, spe_weighted)
-df_temp$sim <- 1
+# plot average TPR and TNR for different FDR levels
+p_TPR <- df_avg %>%
+  ggplot(aes(x = fdr_levels)) +
+  geom_line(aes(y = tpr_levels_unweighted, color = "unweighted")) +
+  geom_line(aes(y = tpr_levels_weighted, color = "weighted")) +
+  ggtitle("Average TPR vs FDR") +
+  theme_bw() +
+  theme(legend.position = "top")
 
-df <- rbind(df, df_temp)
+p_TNR <- df_avg %>%
+  ggplot(aes(x = fdr_levels)) +
+  geom_line(aes(y = tnr_levels_unweighted, color = "unweighted")) +
+  geom_line(aes(y = tnr_levels_weighted, color = "weighted")) +
+  ggtitle("Average TNR vs FDR") +
+  theme_bw() +
+  theme(legend.position = "top")
 
-# each plot should have two lines (one for unweighted and one for weighted)
-# x axis is the FDR level
-# y axis is the TNR or TPR
-# color the lines by the model type (unweighted or weighted)
-# dash or solid lines for TNR and TPR
-
-p <- ggplot(df, aes(x = fdr_levels)) +
-  geom_line(aes(y = tnr_levels_unweighted, color = "unweighted", linetype = "dashed")) +
-  geom_line(aes(y = tnr_levels_weighted, color = "weighted", linetype = "dashed")) +
-  facet_wrap(~sim) +
-  labs(title = "TNR vs FDR",
-       x = "FDR",
-       y = "TNR",
-       color = "Model") +
-  theme_bw()
-
-# save the plot
-ggsave("combined_TNR.png", p, width = 10, height = 10)
-
-p <- ggplot(df, aes(x = fdr_levels)) +
-  geom_line(aes(y = tpr_levels_unweighted, color = "unweighted", linetype = "solid")) +
-  geom_line(aes(y = tpr_levels_weighted, color = "weighted", linetype = "solid")) +
-  facet_wrap(~sim) +
-  labs(title = "TPR vs FDR",
-       x = "FDR",
-       y = "TPR",
-       color = "Model") +
-  theme_bw()
-
-# save the plot
-ggsave("combined_TPR.png", p, width = 10, height = 10)
-
-
-create_p_val_df <- function(spe_unweighted, spe_weighted) {
-  
-  # UNWEIGHTED
-  
-  # Sort unadjusted p-values and calculate FDR
-  p_values_unweighted <- rowData(spe_unweighted)$pval
-  
-  # WEIGHTED
-  
-  # Sort unadjusted p-values and calculate FDR
-  p_values_weighted <- 1 - pchisq(rowData(spe_weighted)$weighted_LR_stat, df = 2)
-  
-  df <- data.frame(p_values_weighted = p_values_weighted,
-                   p_values_unweighted = p_values_unweighted)
-  
-  return(df)
-}
-
-
-# use the function to create the data frame for the files from all the folders from fdr_1 to fdr_8
-# create a column for the fdr_1 to fdr_8 
-
-df <- data.frame()
-for (i in 1:8) {
-  print(i)
-  spe_unweighted <- readRDS(paste0("fdr_", i, "/spe_nnSVG.rds"))
-  spe_weighted <- readRDS(paste0("fdr_", i, "/spe_weighted_nnSVG.rds"))
-  
-  df_temp <- create_p_val_df(spe_unweighted, spe_weighted)
-  df_temp$sim <- i+1
-  
-  df <- rbind(df, df_temp)
-}
-
-# add another df_temp to df using i=1 from the folder "../weight_effect/simulated_data/nonSVGs/simple_300_unif_means/"
-spe_unweighted <- readRDS("../weight_effect/simulated_data/nonSVGs/simple_300_unif_means/spe_nnSVG.rds")
-spe_weighted <- readRDS("../weight_effect/simulated_data/nonSVGs/simple_300_unif_means/spe_weighted_nnSVG.rds")
-
-df_temp <- create_p_val_df(spe_unweighted, spe_weighted)
-df_temp$sim <- 1
-
-df <- rbind(df, df_temp)
-
-# plot p value distributions for sim 1 only
-# two separate plots for weighted and unweighted
-# x axis is the p value
-# y axis is the density
-# color the lines by the model type (unweighted or weighted)
-sub_df <- df[df$sim == 9, ]
-
-p1 <- ggplot(sub_df, aes(x = p_values_unweighted)) +
-  geom_histogram(aes(y = ..density..), bins = 50, color = "black", fill = "blue", alpha = 0.5) +
-  labs(title = "P-value distribution for unweighted model",
-       x = "P-value",
-       y = "Density") +
-  theme_bw()
-
-p2 <- ggplot(sub_df, aes(x = p_values_weighted)) +
-  geom_histogram(aes(y = ..density..), bins = 50, color = "black", fill = "blue", alpha = 0.5) +
-  labs(title = "P-value distribution for weighted model",
-       x = "P-value",
-       y = "Density") +
-  theme_bw()
-
-# save the plots together
-pdf("p_val_distributions_unfiltered_sim9.pdf")
-print(p1)
-print(p2)
+# save plots
+pdf("FDR_TPR_TNR.pdf")
+print(p_FDR)
+print(p_TPR)
+print(p_TNR)
 dev.off()
-
-
-create_filtered_p_val_df <- function(spe_unweighted, spe_weighted) {
-  
-  # UNWEIGHTED
-  
-  # remove lowly expressed genes
-  indices <- rowData(spe_unweighted)$mean > 0.75
-  spe_unweighted <- spe_unweighted[indices, ]
-  sorted_p_values_unweighted <- sort(rowData(spe_unweighted)$pval)
-  
-  # WEIGHTED
-  # remove lowly expressed genes
-  # use unweighted mean to filter out lowly expressed genes since this is the true mean
-  spe_weighted <- spe_weighted[indices, ]
-  pvalues_weighted <- 1 - pchisq(rowData(spe_weighted)$weighted_LR_stat, df = 2)
-  sorted_p_values_weighted <- sort(pvalues_weighted, na.last = TRUE)
-  
-  df <- data.frame(sorted_p_values_weighted = sorted_p_values_weighted,
-                   sorted_p_values_unweighted = sorted_p_values_unweighted)
-  
-  return(df)
-}
-
-
-# use the function to create the data frame for the files from all the folders from fdr_1 to fdr_8
-# create a column for the fdr_1 to fdr_8 
-
-df <- data.frame()
-for (i in 1:8) {
-  print(i)
-  spe_unweighted <- readRDS(paste0("fdr_", i, "/spe_nnSVG.rds"))
-  spe_weighted <- readRDS(paste0("fdr_", i, "/spe_weighted_nnSVG.rds"))
-  
-  df_temp <- create_filtered_p_val_df(spe_unweighted, spe_weighted)
-  df_temp$sim <- i+1
-  
-  df <- rbind(df, df_temp)
-}
-
-# add another df_temp to df using i=1 from the folder "../weight_effect/simulated_data/nonSVGs/simple_300_unif_means/"
-spe_unweighted <- readRDS("../weight_effect/simulated_data/nonSVGs/simple_300_unif_means/spe_nnSVG.rds")
-spe_weighted <- readRDS("../weight_effect/simulated_data/nonSVGs/simple_300_unif_means/spe_weighted_nnSVG.rds")
-
-df_temp <- create_filtered_p_val_df(spe_unweighted, spe_weighted)
-df_temp$sim <- 1
-
-df <- rbind(df, df_temp)
-
-# plot p value distributions for sim 1 only
-# two separate plots for weighted and unweighted
-# x axis is the p value
-# y axis is the density
-# color the lines by the model type (unweighted or weighted)
-sub_df <- df[df$sim == 1, ]
-
-p1 <- ggplot(sub_df, aes(x = sorted_p_values_unweighted)) +
-  geom_histogram(aes(y = ..density..), bins = 50, color = "black", fill = "blue", alpha = 0.5) +
-  labs(title = "P-value distribution for unweighted model",
-       x = "P-value",
-       y = "Density") +
-  theme_bw()
-
-p2 <- ggplot(sub_df, aes(x = sorted_p_values_weighted)) +
-  geom_histogram(aes(y = ..density..), bins = 50, color = "black", fill = "blue", alpha = 0.5) +
-  labs(title = "P-value distribution for weighted model",
-       x = "P-value",
-       y = "Density") +
-  theme_bw()
-
-# save the plots together
-pdf("p_val_distributions_sim1_filtered.pdf")
-print(p1)
-print(p2)
-dev.off()
-
