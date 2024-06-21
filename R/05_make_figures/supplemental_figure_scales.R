a70e4d60fb9347482f4aa0dd45ebd29e7de497d3
Stephanie Hicks <stephaniechicks@gmail.com>
2024-06-21 12:49:27 -0400

    using sample mean and sample var for both

--- R/05_make_figures/supplemental_figure_scales.R
+++ R/05_make_figures/supplemental_figure_scales.R
@@ -5,10 +5,11 @@ library(patchwork)
 library(scuttle)
 library(here)
 
+# load data: simulated counts from splatter (10k genes, 100 cells, default parameters)
 fn <- here("outputs", "preprocessed", "sce_simulated_Gamma-Poisson_preprocessed.rds")
 sim <- readRDS(fn)
 
-### calculate empirical mean and variance 
+### calculate sample mean and variance 
 emp_mean <- rowMeans(counts(sim))
 emp_var <- rowVars(counts(sim))
 
@@ -16,17 +17,21 @@ p1 <- data.frame(x=emp_mean, y=emp_var) %>%
   ggplot() +
   geom_point(aes(x=x, y=y)) +
   theme_bw() +
-  labs(x = "empirical mean", y="empirical variance", title = "Empirical scale")
+  labs(x = "sample mean", y="sample variance", 
+       title = "Using raw counts")
 
 # voom plot
 sim <- logNormCounts(sim)
-emp_var_log <- rowVars(logcounts(sim))
+emp_var_log <- rowVars(logcounts(sim)) # sample variance of log-counts
+emp_mean_log <- rowMeans(logcounts(sim)) # sample mean of log-counts
 
-p2 <- data.frame(x=log2(emp_mean + 0.5), y=sqrt(emp_var_log)) %>%
+p2 <- data.frame(x=emp_mean_log,
+                 y=emp_var_log) %>%
   ggplot() +
   geom_point(aes(x=x, y=y)) +
   theme_bw() +
-  labs(x = "log2(empirical mean)", y="std. dev. logcounts", title = "log2 scale")
+  labs(x = "sample mean", y="standard variance", 
+       title = "Using log-counts")
 
 fig1 <- wrap_plots(p1, p2, nrow=1)
 ggsave(here("plots", "supplementary", "scales.png"), fig1)
