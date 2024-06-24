6624e20c64a1d95577f0cd8cbdab9a5a30ce435e
Stephanie Hicks <stephaniechicks@gmail.com>
2024-06-24 16:18:00 -0400

    minor edits to org of nnsvg on dlpfc dataset

--- R/02_run_methods/run_nnSVG/run_nnSVG_humanDLPFC.R
+++ R/02_run_methods/run_nnSVG/run_nnSVG_humanDLPFC.R
@@ -4,21 +4,33 @@ library(scran)
 library(spatialLIBD)
 library(here)
 
+# ---------
+# load data
+# ---------
+
 fn <- here("outputs", "preprocessed", "spe_humanDLPFC_preprocessed.rds")
 spe <- readRDS(file=fn)
 
+# ---------
+# run nnSVG
+# ---------
+
 set.seed(123)
 spe <- nnSVG(
-	     spe,
-	     X = NULL,
-	     assay_name = "logcounts",
-	     n_neighbors = 10,
-	     order = "AMMD",
-	     n_threads = 10,
-	     verbose = FALSE
+  spe,
+  X = NULL,
+  assay_name = "logcounts",
+  n_neighbors = 10,
+  order = "AMMD",
+  n_threads = 10,
+  verbose = FALSE
 )
 
-file = here("outputs","results","spe_humanDLPFC_nnSVG.rds"
+# -----------
+# save object
+# -----------
+
+file = here("outputs","results","spe_humanDLPFC_nnSVG.rds")
 saveRDS(spe, file = file)
 
 
