12b7fdf0fd964bd67e3f77e7921882be10ba4f3b
Kinnary Shah <kinnaryshahh@gmail.com>
2024-06-26 12:51:30 -0400

    convert from DelayedMatrix to dgCMatrix

--- R/02_run_methods/run_weighted_nnSVG/run_weighted_nnSVG_humanOvarian.R
+++ R/02_run_methods/run_weighted_nnSVG/run_weighted_nnSVG_humanOvarian.R
@@ -16,6 +16,18 @@ fn <- here("outputs", "preprocessed", "spe_humanOvarian_preprocessed.rds")
 spe <- readRDS(file=fn)
 
 # ---------
+# convert to dgCMatrix
+# ---------
+
+regular_matrix_counts <- as.matrix(assays(spe)[["counts"]])
+sparse_matrix_counts <- as(regular_matrix_counts, "dgCMatrix")
+assays(spe)$counts <- sparse_matrix_counts
+
+regular_matrix_logcounts <- as.matrix(assays(spe)[["logcounts"]])
+sparse_matrix_logcounts <- as(regular_matrix_logcounts, "dgCMatrix")
+assays(spe)$logcounts <- sparse_matrix_logcounts
+
+# ---------
 # run weighted nnSVG
 # ---------
 
