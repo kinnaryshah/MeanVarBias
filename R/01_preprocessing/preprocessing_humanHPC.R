2594bb0f5e6a2548e7c37c8d16cccb35fecbf100
Stephanie Hicks <stephaniechicks@gmail.com>
2024-06-28 01:12:54 -0400

    org human HPC file

--- R/01_preprocessing/preprocessing_humanHPC.R
+++ R/01_preprocessing/preprocessing_humanHPC.R
@@ -17,6 +17,10 @@ spe <- read10xVisium(here("outputs/raw/humanHPC/outs/"),
                      images = "lowres", # specify which image(s) to include
                      load = TRUE)      # specify whether or not to load image(s)
 
+# -------------
+# preprocessing
+# -------------
+
 #subset to one sample
 spe <- spe[, spe$sample_id == "V10B01-085_A1"]
 
@@ -24,8 +28,19 @@ spe <- filter_genes(spe)
 
 spe <- logNormCounts(spe)
 
+
+# -----------
+# save object
+# -----------
+
 fn <- here("outputs", "preprocessed", "spe_humanHPC_preprocessed.rds")
 saveRDS(spe, file = fn)
+
+# -----------
+# session information
+# -----------
+
+sessionInfo()
 
 
 
