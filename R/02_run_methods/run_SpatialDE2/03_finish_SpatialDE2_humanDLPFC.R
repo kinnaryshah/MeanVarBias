845c86c3c4cda535fa83ca65e6676116d5c33099
Kinnary Shah <kinnaryshahh@gmail.com>
2024-07-01 10:02:34 -0400

    finish SpatialDE2 save in spe

---
+++ R/02_run_methods/run_SpatialDE2/03_finish_SpatialDE2_humanDLPFC.R
@@ -0,0 +1,33 @@
+library(SpatialExperiment)
+library(here)
+
+# ---------
+# load data
+# ---------
+
+fn <- here("outputs", "preprocessed", "spe_humanDLPFC_preprocessed.rds")
+spe <- readRDS(file=fn)
+
+fn <- here("outputs", "results", "SpatialDE2_results_humanDLPFC.csv")
+results <- read.csv(file=fn, header = T, sep = ",", row.names = 1)
+
+# ---------
+# merge results into spe
+# ---------
+
+df_merge <- merge(rowData(spe), results, by=0)
+row.names(df_merge) <- df_merge$Row.names
+df_merge$Row.names <- NULL
+
+# rank largest fraction of spatial variances on top
+df_merge$SpatialDE2_rank <- rank(-df_merge$FSV, ties.method = "first")
+
+# add to spe
+rowData(spe) <- df_merge
+
+# ---------
+# save spe
+# ---------
+
+fn <- here("outputs", "results", "spe_humanDLPFC_SpatialDE2.rds")
+saveRDS(spe, file=fn)
--- R/02_run_methods/run_SpatialDE2/run_SpatialDE2_humanDLPFC.sh
+++ R/02_run_methods/run_SpatialDE2/run_SpatialDE2_humanDLPFC.sh
@@ -6,9 +6,14 @@
 #SBATCH --error=run_SpatialDE2_humanDLPFC.e
 #SBATCH --job-name=SpatialDE2_humanDLPFC
 
+# pip install git+https://github.com/PMBio/SpatialDE
+# installed in virtual environment named spatialde2_env
+
 module load conda_R/4.4.x
 Rscript 01_prep_humanDLPFC.R
 
 module load conda
 conda activate spatialde2_env
 python 02_run_SpatialDE2_humanDLPFC.py
+
+Rscript 03_finish_SpatialDE2_humanDLPFC.R
