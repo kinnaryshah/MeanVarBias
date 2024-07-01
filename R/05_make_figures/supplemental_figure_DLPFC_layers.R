a3539b22205760cbcc1af8eb5a4ad33925a7e42b
Kinnary Shah <kinnaryshahh@gmail.com>
2024-07-01 14:54:39 -0400

    create supp figure 2

---
+++ R/05_make_figures/supplemental_figure_DLPFC_layers.R
@@ -0,0 +1,106 @@
+library(SpatialExperiment)
+library(here)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(ggplot2)
+library(ggrepel)
+library(viridis)
+library(ggpubr)
+library(purrr)
+library(patchwork)
+library(here)
+
+df_fxn <- function(layer) {
+  
+  file = here("outputs","results",paste0("spe_humanDLPFC_layer_", layer, "_nnSVG.rds"))
+  spe <- readRDS(file = file_name)
+  
+  spe_df <- rowData(spe)
+  
+  df_nnSVG <- 
+    as.data.frame(spe_df)
+  
+  df_effect <- 
+    as.data.frame(df_nnSVG) %>% 
+    mutate(l = 1 / phi) %>% 
+    filter(rank <= 1000)
+  
+  df_effect$LR_stat_scaled <- df_effect$LR_stat / max(df_effect$LR_stat)
+  
+  #add col of layer name
+  df_effect$layer_val <- rep(layer,1000)
+  return(df_effect)
+}
+
+layer_list <- c("L1", "L2", "L3", "L4", "L5", "L6", "WM")
+#create dfs of all layers
+layer_df <- map(.x=layer_list, .f=~df_fxn(.x))
+#convert to df and rbind, will facet on layer column which was added in df_fxn
+all_layers_df <- Reduce(rbind,layer_df)
+
+# variance vs. mean
+var <- ggplot(all_layers_df, 
+              aes(x = mean, y = var, color = LR_stat_scaled)) + 
+  geom_point(size = 1) + 
+  geom_smooth(method="loess", color="black", size=0.5) +
+  facet_grid(layer_val~., switch = "y") +
+  scale_color_viridis(trans = "log10") +
+  scale_color_gradient(low = "blue", high = "red") +
+  labs(x = "mean logcounts", 
+       y = "", 
+       color = "LR stat",
+       title = expression(sigma^{2}+tau^{2})) + 
+  theme_bw() + 
+  theme(strip.text.y.left = element_text(angle = 0)) +
+  theme(legend.key = element_blank(), strip.background = element_rect(colour="black", fill="white") ) +
+  theme(axis.title.x = element_text(size = 7))
+
+# spatial variance vs. mean
+spat_var <- ggplot(all_layers_df, 
+                   aes(x = mean, y = sigma.sq, color = LR_stat_scaled)) + 
+  geom_point(size = 1) + 
+  facet_grid(layer_val~., switch = "y") +
+  scale_color_viridis(trans = "log10") +
+  scale_color_gradient(low = "blue", high = "red") +
+  labs(x = "mean logcounts", 
+       y = "", 
+       color = "LR stat",
+       title = expression(sigma^{2})) + 
+  theme_bw() + 
+  theme(strip.background = element_blank(), strip.text.y = element_blank()) +
+  theme(axis.title.x = element_text(size = 7))
+
+nonspat_var <- ggplot(all_layers_df, 
+                      aes(x = mean, y = tau.sq, color = LR_stat_scaled)) + 
+  geom_point(size = 1) + 
+  facet_grid(layer_val~., switch = "y") +
+  scale_color_viridis(trans = "log10") +
+  scale_color_gradient(low = "blue", high = "red") +
+  labs(x = "mean logcounts", 
+       y = "", 
+       color = "LR stat",
+       title =expression(tau^{2})) + 
+  theme_bw() + 
+  theme(strip.background = element_blank(), strip.text.y = element_blank()) +
+  theme(axis.title.x = element_text(size = 7))
+
+prop <- ggplot(all_layers_df, 
+               aes(x = mean, y = prop_sv, color = LR_stat_scaled)) + 
+  geom_point(size = 1) + 
+  facet_grid(layer_val~., switch = "y") +
+  scale_color_viridis(trans = "log10") +
+  scale_color_gradient(low = "blue", high = "red") +
+  labs(x = "mean logcounts", 
+       y = "", 
+       color = "LR stat",
+       title = expression(sigma^{2}/(sigma^{2}+tau^{2}))) + 
+  theme_bw() + 
+  theme(strip.background = element_blank(), strip.text.y = element_blank()) +
+  theme(axis.title.x = element_text(size = 7))
+
+pdf(here("plots", "supplementary", "DLPFC_layers.png"))
+wrap_plots(var, spat_var, nonspat_var, prop, guides="collect",
+           ncol=4, nrow=1)
+dev.off()
+
