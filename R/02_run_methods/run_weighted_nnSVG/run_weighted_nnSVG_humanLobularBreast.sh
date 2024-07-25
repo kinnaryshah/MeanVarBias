7837c204c30ae5a2220026408df16c6ee8962cd9
kinnaryshah <kinnaryshahh@gmail.com>
2024-07-25 12:22:38 -0400

    add more time

--- R/02_run_methods/run_weighted_nnSVG/run_weighted_nnSVG_humanLobularBreast.sh
+++ R/02_run_methods/run_weighted_nnSVG/run_weighted_nnSVG_humanLobularBreast.sh
@@ -2,6 +2,7 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=5G
 #SBATCH --mail-type=END
+#SBATCH --time=2-0:00:00
 #SBATCH --output=run_weighted_nnSVG_humanLobularBreast.o
 #SBATCH --error=run_weighted_nnSVG_humanLobularBreast.e
 #SBATCH --job-name=weighted_nnSVG_humanLobularBreast
