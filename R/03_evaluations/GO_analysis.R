798e5087aa383c87a1b598aed63de12a37309835
Kinnary Shah <kinnaryshahh@gmail.com>
2024-08-14 16:15:34 -0400

    re-try GO with top 1000 SVGs

--- R/03_evaluations/GO_analysis.R
+++ R/03_evaluations/GO_analysis.R
@@ -135,3 +135,82 @@ for (d in dat) {
   dev.off()
   
 }
+
+
+# above didn't work so well 
+# now let's try just the top 1000 ranked SVGs for weighted vs unweighted
+
+get_gene_lists <- function(spe_unweighted, spe_weighted) {
+  
+  # get the gene list for the unweighted spe rank <= 1000
+  # gene id is in rowData(spe_unweighted)$gene_id
+  gene_list_unweighted <- rowData(spe_unweighted)$gene_id[which(rowData(spe_unweighted)$rank <= 1000)]
+  
+  # get the gene list for the weighted spe using rowData(spe_weighted)$weighted_rank <= 1000
+  gene_list_weighted <- rowData(spe_weighted)$gene_id[which(rowData(spe_weighted)$weighted_rank <= 1000)]
+  
+  # return the gene list for the unweighted and weighted spe
+  return(list(gene_list_unweighted, gene_list_weighted))
+  
+}
+
+dat=c("LobularBreast") 
+
+for (d in dat) {
+  print(d)
+  spe_unweighted <- readRDS(here("outputs", "results", paste0("spe_human", d, "_nnSVG.rds")))
+  spe_weighted <- readRDS(here("outputs", "results", paste0("spe_human", d, "_weighted_nnSVG.rds")))
+  gene_lists <- get_gene_lists(spe_unweighted, spe_weighted)
+  
+  gene_list1_entrez <- bitr(gene_lists[[1]], fromType = "ENSEMBL", toType = "ENTREZID", OrgDb = org.Hs.eg.db)
+  gene_list2_entrez <- bitr(gene_lists[[2]], fromType = "ENSEMBL", toType = "ENTREZID", OrgDb = org.Hs.eg.db)
+  
+  gene_list <- list(unweighted = gene_list1_entrez$ENTREZID, weighted = gene_list2_entrez$ENTREZID)
+  
+  compare_ego_bp  <- compareCluster(geneCluster = gene_list,
+                                         fun = "enrichGO",
+                                         OrgDb = org.Hs.eg.db,
+                                         ont = "BP",
+                                         pAdjustMethod = "BH",
+                                         pvalueCutoff = 0.05,
+                                         qvalueCutoff = 0.1,
+                                         readable = TRUE)
+  
+  compare_ego_cc  <- compareCluster(geneCluster = gene_list,
+                                         fun = "enrichGO",
+                                         OrgDb = org.Hs.eg.db,
+                                         ont = "CC",
+                                         pAdjustMethod = "BH",
+                                         pvalueCutoff = 0.05,
+                                         qvalueCutoff = 0.1,
+                                         readable = TRUE)
+  
+  compare_ego_mf  <- compareCluster(geneCluster = gene_list,
+                                         fun = "enrichGO",
+                                         OrgDb = org.Hs.eg.db,
+                                         ont = "MF",
+                                         pAdjustMethod = "BH",
+                                         pvalueCutoff = 0.05,
+                                         qvalueCutoff = 0.1,
+                                         readable = TRUE)
+  
+  compare_ego_do  <- compareCluster(geneCluster = gene_list,
+                                         fun = "enrichDO",
+                                         ont = "DO",
+                                         pAdjustMethod = "BH",
+                                         pvalueCutoff = 0.05,
+                                         qvalueCutoff = 0.1,
+                                         readable = TRUE)
+  
+
+  pdf(here("plots", "GO_comparison", paste0("GO_comparison_top1000_", d, ".pdf")))
+  
+  # Visualize the comparison results
+  dotplot(compare_ego_bp ) + ggtitle(paste0(d, " BP Enrichment Comparison "))
+  dotplot(compare_ego_cc ) + ggtitle(paste0(d, " CC Enrichment Comparison "))
+  dotplot(compare_ego_mf ) + ggtitle(paste0(d, " MF Enrichment Comparison "))
+  dotplot(compare_ego_do ) + ggtitle(paste0(d, " DO Enrichment Comparison "))
+  
+  dev.off()
+  
+}
