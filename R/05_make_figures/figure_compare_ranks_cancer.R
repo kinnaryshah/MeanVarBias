06ffb7c500801b08ef40ee56ca4a047b82ca8428
Kinnary Shah <kinnaryshahh@gmail.com>
2025-02-21 14:12:58 -0500

    vis ranks and color by cancer

---
+++ R/05_make_figures/figure_compare_ranks_cancer.R
@@ -0,0 +1,236 @@
+library(ggplot2)
+library(SpatialExperiment)
+library(patchwork)
+library(GGally)
+library(dplyr)
+library(ggridges)
+library(here)
+library(ggrepel)
+
+My_Theme = theme(
+  axis.title.x = element_text(size = 20),
+  axis.title.y = element_text(size = 20),
+  legend.title = element_text(size=20),
+  legend.text = element_text(size=20),
+  title = element_text(size = 20))
+
+# OVARIAN
+
+spe_nnSVG_Ovarian <- readRDS(here("outputs", "results", "spe_humanOvarian_nnSVG.rds"))
+spe_weighted_nnSVG_Ovarian <- readRDS(here("outputs", "results", "spe_humanOvarian_weighted_nnSVG.rds"))
+
+n_genes_Ovarian <- dim(spe_weighted_nnSVG_Ovarian)[1]
+
+# remove one instance of "HSPA14" for duplicate row names
+idx <- which(rowData(spe_weighted_nnSVG_Ovarian)$gene_name == "HSPA14")[2]
+
+spe_nnSVG_Ovarian <- spe_nnSVG_Ovarian[-idx,]
+spe_weighted_nnSVG_Ovarian <- spe_weighted_nnSVG_Ovarian[-idx,]
+
+df_Ovarian <- data.frame(
+  unweighted = rowData(spe_nnSVG_Ovarian)$rank,
+  weighted = rowData(spe_weighted_nnSVG_Ovarian)$weighted_rank)
+
+rownames(df_Ovarian) <- rowData(spe_weighted_nnSVG_Ovarian)$gene_name
+
+# cancer genes:
+# low lengthscale "LSAMP", "DTX3L", "CASC15", "SLC25A6", "CDK9", "MAP3K8", "GCH1", "PCYT2",
+# low mean "TUFT1", "DDX39B"
+
+# create a column in the df called "cancer"
+# should be true if in the list above and false if not
+
+cancer_genes <- c("LSAMP", "DTX3L", "CASC15", "SLC25A6", "CDK9", "MAP3K8", "GCH1", "PCYT2",
+                  "TUFT1", "DDX39B")
+
+df_Ovarian$cancer <- ifelse(rownames(df_Ovarian) %in% cancer_genes, TRUE, FALSE)
+
+# plot ranks on either axis
+Ovarian_ranks <- ggplot(df_Ovarian, aes(x = unweighted, y = weighted, color = cancer)) +
+  geom_point(size = 0.5) +
+  geom_point(data = filter(df_Ovarian, cancer == T), aes(x = unweighted, y = weighted, color = cancer), size = 2) +
+  geom_text_repel(data = filter(df_Ovarian, cancer == T), 
+                  aes(x = unweighted, y = weighted, label = rownames(filter(df_Ovarian, cancer == T))),
+                  size = 5, max.overlaps = 20, color = "black") + 
+  xlim(c(1, n_genes_Ovarian)) +
+  ylim(c(1, n_genes_Ovarian)) +
+  geom_abline(slope=1, intercept=0) +
+  ggtitle("Ovarian") +
+  theme_bw() +
+  My_Theme
+
+# BREAST
+
+spe_nnSVG_Breast <- readRDS(here("outputs", "results", "spe_humanBreast_nnSVG.rds"))
+spe_weighted_nnSVG_Breast <- readRDS(here("outputs", "results", "spe_humanBreast_weighted_nnSVG.rds"))
+
+n_genes_Breast <- dim(spe_weighted_nnSVG_Breast)[1]
+
+# remove one instance of "HSPA14" for duplicate row names
+idx <- which(rowData(spe_weighted_nnSVG_Breast)$gene_name == "HSPA14")[2]
+
+spe_nnSVG_Breast <- spe_nnSVG_Breast[-idx,]
+spe_weighted_nnSVG_Breast <- spe_weighted_nnSVG_Breast[-idx,]
+
+df_Breast <- data.frame(
+  unweighted = rowData(spe_nnSVG_Breast)$rank,
+  weighted = rowData(spe_weighted_nnSVG_Breast)$weighted_rank)
+
+rownames(df_Breast) <- rowData(spe_weighted_nnSVG_Breast)$gene_name
+
+# cancer genes:
+# low lengthscale "DDX20", "ASXL2", "MGAT5", "ICOS", "TNFRSF21", "NUP43", "CDK13", "NOL6", "DAPK1", "TRIM21",
+# "FAM111B", "SPDYC", "ZBTB1", "PEAK1", "RCCD1", "FTO", "ULK2", "TRIM65", "DNMT3B"
+# low mean "ATXN7", "E2F3", "MFHAS1", "SAAL1", "DDX10", "TLN2", "ITCH", "ZHX3"
+
+# create a column in the df called "cancer"
+# should be true if in the list above and false if not
+
+cancer_genes <- c("DDX20", "ASXL2", "MGAT5", "ICOS", "TNFRSF21", "NUP43", "CDK13", "NOL6", "DAPK1", "TRIM21",
+                  "FAM111B", "SPDYC", "ZBTB1", "PEAK1", "RCCD1", "FTO", "ULK2", "TRIM65", "DNMT3B",
+                  "ATXN7", "E2F3", "MFHAS1", "SAAL1", "DDX10", "TLN2", "ITCH", "ZHX3")
+
+df_Breast$cancer <- ifelse(rownames(df_Breast) %in% cancer_genes, TRUE, FALSE)
+
+# plot ranks on either axis
+Breast_ranks <- ggplot(df_Breast, aes(x = unweighted, y = weighted, color = cancer)) +
+  geom_point(size = 0.5) +
+  geom_point(data = filter(df_Breast, cancer == T), aes(x = unweighted, y = weighted, color = cancer), size = 2) +
+  geom_text_repel(data = filter(df_Breast, cancer == T), 
+                  aes(x = unweighted, y = weighted, label = rownames(filter(df_Breast, cancer == T))),
+                  size = 5, max.overlaps = 20, color = "black") + 
+  xlim(c(1, n_genes_Breast)) +
+  ylim(c(1, n_genes_Breast)) +
+  geom_abline(slope=1, intercept=0) +
+  ggtitle("Ductal Breast") +
+  theme_bw() +
+  My_Theme
+
+ggsave(here("plots", "main", "test_Breast.png"),
+       wrap_plots(
+         Breast_ranks,
+         nrow = 1,
+         guides = "collect",
+         axis_titles = "collect"
+       ) + plot_annotation(tag_levels = 'A') & 
+         theme(plot.tag = element_text(size = 25)),
+       width = 20, height = 20)
+
+# SUBTYPE BREAST
+
+spe_nnSVG_SubtypeBreast <- readRDS(here("outputs", "results", "spe_humanSubtypeBreast_nnSVG.rds"))
+spe_weighted_nnSVG_SubtypeBreast <- readRDS(here("outputs", "results", "spe_humanSubtypeBreast_weighted_nnSVG.rds"))
+
+n_genes_SubtypeBreast <- dim(spe_weighted_nnSVG_SubtypeBreast)[1]
+
+# remove one instance of "HSPA14" for duplicate row names
+idx1 <- which(rowData(spe_weighted_nnSVG_SubtypeBreast)$gene_name == "HSPA14")[2]
+idx2 <- which(rowData(spe_weighted_nnSVG_SubtypeBreast)$gene_name == "CYB561D2")[1]
+
+spe_nnSVG_SubtypeBreast <- spe_nnSVG_SubtypeBreast[-c(idx1,idx2),]
+spe_weighted_nnSVG_SubtypeBreast <- spe_weighted_nnSVG_SubtypeBreast[-c(idx1,idx2),]
+
+df_SubtypeBreast <- data.frame(
+  unweighted = rowData(spe_nnSVG_SubtypeBreast)$rank,
+  weighted = rowData(spe_weighted_nnSVG_SubtypeBreast)$weighted_rank)
+
+rownames(df_SubtypeBreast) <- rowData(spe_weighted_nnSVG_SubtypeBreast)$gene_name
+
+# cancer genes:
+# low lengthscale "CASP8", "ATP11B", "E2F3", "EHMT2", "TREM1", "SMC2", "CD82", "BIRC2", "GABARAPL1",
+# "CDYL2", "MSI2", "CTDP1", "DDA1", "JAM2", "TAF1", "FGF13"
+# low mean NULL
+
+# create a column in the df called "cancer"
+# should be true if in the list above and false if not
+
+cancer_genes <- c("CASP8", "ATP11B", "E2F3", "EHMT2", "TREM1", "SMC2", "CD82", "BIRC2", "GABARAPL1",
+                  "CDYL2", "MSI2", "CTDP1", "DDA1", "JAM2", "TAF1", "FGF13")
+
+df_SubtypeBreast$cancer <- ifelse(rownames(df_SubtypeBreast) %in% cancer_genes, TRUE, FALSE)
+
+# plot ranks on either axis
+SubtypeBreast_ranks <- ggplot(df_SubtypeBreast, aes(x = unweighted, y = weighted, color = cancer)) +
+  geom_point(size = 0.5) +
+  geom_point(data = filter(df_SubtypeBreast, cancer == T), aes(x = unweighted, y = weighted, color = cancer), size = 2) +
+  geom_text_repel(data = filter(df_SubtypeBreast, cancer == T), 
+                  aes(x = unweighted, y = weighted, label = rownames(filter(df_SubtypeBreast, cancer == T))),
+                  size = 5, max.overlaps = 20, color = "black") + 
+  xlim(c(1, n_genes_SubtypeBreast)) +
+  ylim(c(1, n_genes_SubtypeBreast)) +
+  geom_abline(slope=1, intercept=0) +
+  ggtitle("Subtype Breast") +
+  theme_bw() +
+  My_Theme
+
+ggsave(here("plots", "main", "test_SubtypeBreast.png"),
+       wrap_plots(
+         SubtypeBreast_ranks,
+         nrow = 1,
+         guides = "collect",
+         axis_titles = "collect"
+       ) + plot_annotation(tag_levels = 'A') & 
+         theme(plot.tag = element_text(size = 25)),
+       width = 20, height = 20)
+
+# LOBULAR BREAST
+
+spe_nnSVG_LobularBreast <- readRDS(here("outputs", "results", "spe_humanLobularBreast_nnSVG.rds"))
+spe_weighted_nnSVG_LobularBreast <- readRDS(here("outputs", "results", "spe_humanLobularBreast_weighted_nnSVG.rds"))
+
+n_genes_LobularBreast <- dim(spe_weighted_nnSVG_LobularBreast)[1]
+
+# remove one instance of "HSPA14" for duplicate row names
+idx1 <- which(rowData(spe_weighted_nnSVG_LobularBreast)$gene_name == "HSPA14")[2]
+idx2 <- which(rowData(spe_weighted_nnSVG_LobularBreast)$gene_name == "CYB561D2")[1]
+
+spe_nnSVG_LobularBreast <- spe_nnSVG_LobularBreast[-c(idx1,idx2),]
+spe_weighted_nnSVG_LobularBreast <- spe_weighted_nnSVG_LobularBreast[-c(idx1,idx2),]
+
+df_LobularBreast <- data.frame(
+  unweighted = rowData(spe_nnSVG_LobularBreast)$rank,
+  weighted = rowData(spe_weighted_nnSVG_LobularBreast)$weighted_rank)
+
+rownames(df_LobularBreast) <- rowData(spe_weighted_nnSVG_LobularBreast)$gene_name
+
+# cancer genes:
+# low lengthscale "ZNF436", "MTF2", "HAGLR", "SATB1", "CD200", "TRIM59", "UCHL1", "LIFR", "FER",
+# "NFIL3", "ENTPD1", "CLEC2D", "FLT1", "CGRRF1", "TYRO3", "ADAM10", "NLRP1", "HAUS5", "MECP2"
+# low mean "PDK1", "PPAT", "ATP6AP1L", "LYRM7", "TRIM35", "TRIM32", "FRAT1", "TIGAR", "ZNF439", "MOSPD2", "BRWD3"
+
+# create a column in the df called "cancer"
+# should be true if in the list above and false if not
+
+cancer_genes <- c("ZNF436", "MTF2", "HAGLR", "SATB1", "CD200", "TRIM59", "UCHL1", "LIFR", "FER",
+                  "NFIL3", "ENTPD1", "CLEC2D", "FLT1", "CGRRF1", "TYRO3", "ADAM10", "NLRP1", "HAUS5", "MECP2",
+                  "PDK1", "PPAT", "ATP6AP1L", "LYRM7", "TRIM35", "TRIM32", "FRAT1", "TIGAR", "ZNF439", "MOSPD2", "BRWD3")
+
+df_LobularBreast$cancer <- ifelse(rownames(df_LobularBreast) %in% cancer_genes, TRUE, FALSE)
+
+# plot ranks on either axis
+LobularBreast_ranks <- ggplot(df_LobularBreast, aes(x = unweighted, y = weighted, color = cancer)) +
+  geom_point(size = 0.5) +
+  geom_point(data = filter(df_LobularBreast, cancer == T), aes(x = unweighted, y = weighted, color = cancer), size = 2) +
+  geom_text_repel(data = filter(df_LobularBreast, cancer == T), 
+                  aes(x = unweighted, y = weighted, label = rownames(filter(df_LobularBreast, cancer == T))),
+                  size = 5, max.overlaps = 20, color = "black") + 
+  xlim(c(1, n_genes_LobularBreast)) +
+  ylim(c(1, n_genes_LobularBreast)) +
+  geom_abline(slope=1, intercept=0) +
+  ggtitle("Lobular Breast") +
+  theme_bw() +
+  My_Theme
+
+ggsave(here("plots", "main", "compare_ranks_cancer.png"),
+       wrap_plots(
+         Ovarian_ranks,
+         SubtypeBreast_ranks,
+         Breast_ranks,
+         LobularBreast_ranks,
+         nrow = 2,
+         guides = "collect",
+         axis_titles = "collect"
+       ) + plot_annotation(tag_levels = 'A') & 
+         theme(plot.tag = element_text(size = 25)),
+       width = 20, height = 20)
+
